b5b797e12be89ca69d7479d2dfc4c19cef2d2eae
mehrdad allahkarami <mehrdadallahkarami@gmail.com>
2016-05-31 19:21:59 +0430

    origin

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ var FileStreamRotator = require('file-stream-rotator');
 var fs = require('fs');
 var path = require('path');
 var http = require('http');
+var url = require("url");
+var queryString = require("querystring");
 var app = express();
-app.use(bodyParser.urlencoded());
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('port', (process.env.PORT || 3000));
 app.use(express.static(path.join(__dirname, 'client')));
 
@@ -23,17 +24,21 @@ server.listen(app.get('port'), function () {
 /*------------------Sample Data--------------------------------*/
 var data = {
     suites: [{
-        id: 0, title: "A Good Sweet", description: "Good Sweet to rent description",
+        id: 0,
+        title: "A Good Sweet",
+        description: "Good Sweet to rent description",
         created_date: "12569537329",
         modified_added: "12569537329",
         access_date: "12569537329",
         country: "Iran",
         city: "Hamedan",
-        price: 240000, area: 124, nor: 2,
+        price: 240000,
+        area: 124,
+        nor: 2,
         address: "No.1, Buali Ave.",
-        vote_up: 10,
+        vote_up: 11,
         vote_down: 1,
-        average_rate: 9,
+        average_rate: 10,
         comments: [{id: 0, comment: "Very good Suite"}
             , {id: 1, comment: "I liked it"}],
         viewed: 10,
@@ -72,6 +77,9 @@ var data = {
             city: "Hamedan",
             address: "No.1,Buali Ave.",
             vote_up: 10, vote_down: 1, average_rate: 9,
+            price: 240000,
+            area: 93,
+            nor: 2,
             comments: [{id: 0, comment: "Very good Suite"}
                 , {id: 1, comment: "I liked it"}],
             viewed: 10,
@@ -88,9 +96,12 @@ var data = {
             modified_added: "12569537329",
             access_date: "12569537329",
             country: "Iran",
-            city: "Hamedan",
+            city: "Tehran",
             address: "No.1,Buali Ave.",
             vote_up: 10, vote_down: 1, average_rate: 9,
+            price: 240000,
+            area: 24,
+            nor: 2,
             comments: [{id: 0, comment: "Very good Suite"}
                 , {id: 1, comment: "I liked it"}],
             viewed: 10,
@@ -108,9 +119,12 @@ var data = {
             modified_added: "12569537329",
             access_date: "12569537329",
             country: "Iran",
-            city: "Hamedan",
+            city: "Isfahan",
             address: "No.1,Buali Ave.",
             vote_up: 10, vote_down: 1, average_rate: 9,
+            price: 240000,
+            area: 114,
+            nor: 2,
             comments: [{id: 0, comment: "Very good Suite"}
                 , {id: 1, comment: "I liked it"}],
             viewed: 10,
@@ -128,9 +142,12 @@ var data = {
             modified_added: "12569537329",
             access_date: "12569537329",
             country: "Iran",
-            city: "Hamedan",
+            city: "Shiraz",
             address: "No.1,Buali Ave.",
             vote_up: 10, vote_down: 1, average_rate: 9,
+            price: 240000,
+            area: 44,
+            nor: 2,
             comments: [{id: 0, comment: "Very good Suite"}
                 , {id: 1, comment: "I liked it"}],
             viewed: 10,
@@ -148,9 +165,12 @@ var data = {
             modified_added: "12569537329",
             access_date: "12569537329",
             country: "Iran",
-            city: "Hamedan",
+            city: "Shiraz",
             address: "No.1,Buali Ave.",
             vote_up: 10, vote_down: 1, average_rate: 9,
+            price: 240000,
+            area: 60,
+            nor: 2,
             comments: [{id: 0, comment: "Very good Suite"}
                 , {id: 1, comment: "I liked it"}],
             viewed: 10,
@@ -171,6 +191,9 @@ var data = {
             city: "Hamedan",
             address: "No.1,Buali Ave.",
             vote_up: 10, vote_down: 1, average_rate: 9,
+            price: 240000,
+            area: 91,
+            nor: 2,
             comments: [{id: 0, comment: "Very good Suite"}
                 , {id: 1, comment: "I liked it"}],
             viewed: 10,
@@ -188,9 +211,12 @@ var data = {
             modified_added: "12569537329",
             access_date: "12569537329",
             country: "Iran",
-            city: "Hamedan",
+            city: "Tehran",
             address: "No.1,Buali Ave.",
             vote_up: 10, vote_down: 1, average_rate: 9,
+            price: 240000,
+            area: 34,
+            nor: 2,
             comments: [{id: 0, comment: "Very good Suite"}
                 , {id: 1, comment: "I liked it"}],
             viewed: 10,
@@ -208,9 +234,12 @@ var data = {
             modified_added: "12569537329",
             access_date: "12569537329",
             country: "Iran",
-            city: "Hamedan",
+            city: "Tehran",
             address: "No.1,Buali Ave.",
             vote_up: 10, vote_down: 1, average_rate: 9,
+            price: 240000,
+            area: 45,
+            nor: 2,
             comments: [{id: 0, comment: "Very good Suite"}
                 , {id: 1, comment: "I liked it"}],
             viewed: 10,
@@ -228,9 +257,12 @@ var data = {
             modified_added: "12569537329",
             access_date: "12569537329",
             country: "Iran",
-            city: "Hamedan",
+            city: "Mashhad",
             address: "No.1,Buali Ave.",
             vote_up: 10, vote_down: 1, average_rate: 9,
+            price: 240000,
+            area: 104,
+            nor: 2,
             comments: [{id: 0, comment: "Very good Suite"}
                 , {id: 1, comment: "I liked it"}],
             viewed: 10,
@@ -253,6 +285,9 @@ var data = {
             city: "Hamedan",
             address: "No.1,Buali Ave.",
             vote_up: 10, vote_down: 1, average_rate: 9,
+            price: 240000,
+            area: 122,
+            nor: 2,
             comments: [{id: 0, comment: "Very good Suite"}
                 , {id: 1, comment: "I liked it"}],
             viewed: 10,
@@ -293,11 +328,9 @@ function assignId(req, res, next) {
 logger.token('id', function getId(req) {
     return req.id;
 });
-
 /*----------Server API -----------------------------*/
 app.get('/all', function (req, res) {
-    var json = JSON.stringify(data);
-    res.status(200).send(json);
+    res.status(200).json(data);
 });
 
 app.get('/suite/:id', function (req, res) {
@@ -305,9 +338,7 @@ app.get('/suite/:id', function (req, res) {
         return res.status(403).send('{error:"unauthorized"}');
     }
     var id = req.params.id;
-    console.log(id);
-    var json = JSON.stringify(data.suites[id]);
-    res.status(200).send(json);
+    res.status(200).json(data.suites[id]);
 });
 
 app.post('/vote_up', function (req, res) {
@@ -319,8 +350,7 @@ app.post('/vote_up', function (req, res) {
         data.suites[id].vote_up += 1;
         data.suites[id].average_rate = (parseInt(data.suites[id].vote_up) - parseInt(data.suites[id].vote_down));
     }
-    var json = JSON.stringify(data.suites[id]);
-    res.status(200).send(json);
+    res.status(200).json(data.suites[id]);
 });
 
 app.post('/vote_down', function (req, res) {
@@ -332,8 +362,7 @@ app.post('/vote_down', function (req, res) {
         data.suites[id].vote_down += 1;
         data.suites[id].average_rate = (parseInt(data.suites[id].vote_up) - parseInt(data.suites[id].vote_down));
     }
-    var json = JSON.stringify(data.suites[id]);
-    res.status(200).send(json);
+    res.status(200).json(data.suites[id]);
 });
 
 app.post('/comment', function (req, res) {
@@ -345,8 +374,7 @@ app.post('/comment', function (req, res) {
     if (data.suites[id]) {
         data.suites[id].comments.push({id: data.suites[id].comments.length, comment: comment});
     }
-    var json = JSON.stringify(data.suites[id]);
-    res.status(200).send(json);
+    res.status(200).send(data.suites[id]);
 });
 
 app.get('/delete_suite', function (req, res) {
@@ -361,10 +389,71 @@ app.post('/update_suite', function (req, res) {
 
 });
 
-app.get('/search_suite', function (req, res) {
-    var search = {
-        // id: 'id', title: '', description: '', vote_up: 1, vote_down: 10, average: 10,
+app.post('/search_suite', function (req, res) {
+    var theUrl = url.parse(req.url);
+    var queryObj = queryString.parse(theUrl.query);
+    var obj = JSON.parse(queryObj.search);
+    var result = {
+        suites: []
     };
+
+    var wrapFunction = function (data) {
+        var found = false;
+        result.suites.map(function (info) {
+            if (info.id === data.id) {
+                found = true;
+            }
+        });
+        if (!found) {
+            result.suites.push(data);
+        }
+    };
+
+    data.suites.map(function (value) {
+        if (obj.hasOwnProperty('average_rate')) {
+            if (value.average_rate === obj.average_rate) {
+                wrapFunction(value);
+            }
+        }
+        if (obj.hasOwnProperty('area_start') && obj.hasOwnProperty('area_end')) {
+            if (value.area >= obj.area_start && value.area <= obj.area_end) {
+                wrapFunction(value);
+            }
+        }
+        if (obj.hasOwnProperty('price_start') && obj.hasOwnProperty('price_end')) {
+            if (value.price >= obj.price_start && value.price <= obj.price) {
+                wrapFunction(value);
+            }
+        }
+        if (obj.hasOwnProperty('nor')) {
+            if (value.nor === obj.nor) {
+                wrapFunction(value);
+            }
+        }
+        if (obj.hasOwnProperty('city')) {
+            var expression = /\obj.city/gi;
+            if (value.city.indexOf(obj.city) || value.city.match(expression)) {
+                wrapFunction(value);
+            }
+        }
+        if (obj.hasOwnProperty('country')) {
+            if (value.country.indexOf(obj.country)) {
+                wrapFunction(value);
+            }
+        }
+        if (obj.hasOwnProperty('title')) {
+            if (value.title.indexOf(obj.title)) {
+                wrapFunction(value);
+            }
+        }
+    });
+
+    res.json(result);
+    /*data.suites.map(function (value) {
+     console.log('----------');
+     console.log(value);
+     });*/
+
 });
 
 app.post('/add_suite', function (req, res) {
